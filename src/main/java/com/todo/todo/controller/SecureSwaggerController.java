f870a3511d75750e2c88bc52d6efc365fc661989
Jith Stephen <jithstephen@Jiths-MacBook-Air.local>
2025-07-08 16:36:20 +0530

    Clean slate: deleted all files in main branch

--- src/main/java/com/todo/todo/controller/SecureSwaggerController.java
+++
@@ -1,7 +0,0 @@
-package com.todo.todo.controller;
-
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-
-@SecurityRequirement(name = "bearerAuth")
-public interface SecureSwaggerController {
-}
